9351b48bfa19fa60c0325270aa7a20bc03a5c07b
Shankarsan Ganai <shankershawn@gmail.com>
2024-06-15 11:07:25 +0530

    first commit

---
+++ src/test/java/com/shankarsan/ai/AiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shankarsan.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertNotNull("hello");
+	}
+
+}
